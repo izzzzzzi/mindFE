cb3f029dcdce36a1e05160f7d3402bb9f2eab7a3
izzzzzzi <noeymaiih@gmail.com>
2021-05-24 22:40:10 +0900

    separate from components from router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import router from './router'
+import App from './App.vue'
 
 new Vue ({
   // el 속성을 통해 뷰 인스턴스가 그려질 지점을 지정
   el: '#app',
   router,
-  render: h => h({template: '<router-view></router-view>'})
+  render: h => h(App)
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import App from '../App.vue'
+import Home from '../components/Home.vue'
+import Login from '../components/Login.vue'
+import NotFound from '../components/NotFound.vue'
 
 Vue.use(VueRouter);
-
-// 컴포넌트 생성
-const Login = { template : '<div>Login Page</div>'}
-const NotFound = { template : '<div>Page Not Found</div>'}
 
 const router = new VueRouter({
   // url 해시 싸용 안함 
   mode: 'history',
   routes : [
-    { path : '/', component : App },
+    { path : '/', component : Home },
     { path : '/login', component : Login },
     { path : '*', component : NotFound}
   ]
