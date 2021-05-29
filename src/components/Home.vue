16977a0e0e538ce64eca809b4f1591b9d622b47e
izzzzzzi <noeymaiih@gmail.com>
2021-05-29 19:20:27 +0900

    use axios

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,9 +2,18 @@
   <div>Hello Home
     <div>
       Board List:
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        API result: <pre>{{apiRes}}</pre>
+      </div>
+      <div v-if="error">
+        <pre>{{error}}</pre>
+      </div>
       <ul>
-        <li class="list">
+        <li>
           <router-link to="/b/1">Board 1</router-link>
+        </li>  
+        <li>      
           <router-link to="/b/2">Board 2</router-link>
         </li>
       </ul>
@@ -13,13 +22,38 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  
+  data(){
+    return{
+      loading: false,
+      apiRes: '',
+      error: '',
+    }
+  },
+  created(){
+    this.fetchData()
+  },
+  methods:{
+    fetchData(){
+      this.loading = true
+
+      axios.get('http://localhost:3000/health')
+      .then(res =>{
+        this.apiRes = res.data
+      })
+      .catch(res => {
+        this.error = res.response.data
+      })
+      .finally(() => {
+        this.loading = false
+      })
+    }
+  }
 }
 </script>
 
 <style>
-.list{
-  display: inline-block;
-}
+
 </style>
